d1af46a693af482a091212ce243552adfebefe13
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-06-24 17:36:42 -0600

    feat: enable cors

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Infrastructure.web_service.routes.pivoting_route import router
 
 
-
 app = FastAPI(title='Métodos numéricos Api', description='Grupo 6: Técnicas de pivoteo')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["POST"],
+)
+
 app.include_router(router, prefix='/api')
