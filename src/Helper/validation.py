10648298b2aa4fe31b28542d2b61510b5f55d29d
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-07-16 02:48:33 -0600

    feat: add parcial pivoting

--- src/Helper/validation.py
+++ src/Helper/validation.py
@@ -5,14 +5,14 @@ def is_square_matriz(data):
   len_of_matriz = len(data.matrix)
   len_of_vector = len(data.vector)
   len_of_unknowns = len(data.unknowns)
-  if(len_of_matriz == 0):
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Empty matrix")
+  if(len_of_matriz == 1):
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You must provide more than one element, matrix")
   
-  if(len_of_vector == 0):
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Empty vector")
+  if(len_of_vector == 1):
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You must provide more than one element, vector")
 
-  if(len_of_unknowns == 0):
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Empty unknowns")
+  if(len_of_unknowns == 1):
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You must provide more than one element, unknowns")
 
   for row in data.matrix:
     if(len(row) != len_of_matriz):
--- src/Infrastructure/web_service/dto/pivoting_request.py
+++ src/Infrastructure/web_service/dto/pivoting_request.py
@@ -3,8 +3,17 @@ from typing import List
 
 class PivotingRequest(BaseModel):
   unknowns: List[str]
-  vector:  List[int]
+  vector:  List[List[float]]
   matrix: List[List[float]]
   
+  example = {
+    "matrix" :[
+      [ 0,   -2,  -1], 
+      [ 2,   3,  1],
+      [ 3,   1,  -1]
+    ], 
+    "vector": [[-14], [1], [1]], 
+    "unknowns": ['x', 'y', 'z']
+  }
       
   
